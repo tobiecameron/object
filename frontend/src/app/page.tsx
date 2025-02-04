f0a9f09b45fd84213b5854f5e4e2f6b19bafd180
tobiecameron <tobie@here.net.au>
2025-02-04 21:44:44 +1300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,35 +1,19 @@
 import Image from "next/image"
-import { client } from "../lib/sanity"
-import Link from "next/link"
 
-async function getHomePageData() {
-  return client.fetch(`{
-    "pages": *[_type == "page"]{
-      title,
-      slug
-    },
-    "homeContent": *[_type == "homepage"][0]{
-      title,
-      subtitle
-    }
-  }`)
-}
-
-export default async function Home() {
-  const { pages, homeContent } = await getHomePageData()
-
+export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <h1 className="text-2xl font-bold">{homeContent.title}</h1>
-        <p className="text-lg">{homeContent.subtitle}</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          {pages.map((page: any) => (
-            <li key={page.slug.current} className="mb-2">
-              <Link href={`/${page.slug.current}`}>{page.title}</Link>
-            </li>
-          ))}
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
