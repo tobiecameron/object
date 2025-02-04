39423f275f4b1e652db76618482aa88f34253d7a
tobiecameron <tobie@here.net.au>
2025-02-04 22:29:40 +1300

    Update page.ts

--- vercel-object-app/schemas/page.ts
+++ vercel-object-app/schemas/page.ts
@@ -32,81 +32,11 @@ export default defineType({
               name: "alt",
               type: "string",
               title: "Alternative text",
-              description: "Important for SEO and accessiblity.",
-            },
-            {
-              name: "size",
-              type: "string",
-              title: "Size",
-              options: {
-                list: [
-                  { title: "Small", value: "small" },
-                  { title: "Medium", value: "medium" },
-                  { title: "Large", value: "large" },
-                  { title: "Full Width", value: "full" },
-                ],
-                layout: "radio",
-              },
-            },
-            {
-              name: "position",
-              type: "string",
-              title: "Position",
-              options: {
-                list: [
-                  { title: "Left", value: "left" },
-                  { title: "Center", value: "center" },
-                  { title: "Right", value: "right" },
-                ],
-                layout: "radio",
-              },
+              description: "Important for SEO and accessibility.",
             },
           ],
         },
       ],
     }),
-    defineField({
-      name: "featuredImage",
-      title: "Featured Image",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-      fields: [
-        {
-          name: "alt",
-          type: "string",
-          title: "Alternative text",
-        },
-        {
-          name: "size",
-          type: "string",
-          title: "Size",
-          options: {
-            list: [
-              { title: "Small", value: "small" },
-              { title: "Medium", value: "medium" },
-              { title: "Large", value: "large" },
-              { title: "Full Width", value: "full" },
-            ],
-            layout: "radio",
-          },
-        },
-        {
-          name: "position",
-          type: "string",
-          title: "Position",
-          options: {
-            list: [
-              { title: "Top", value: "top" },
-              { title: "Middle", value: "middle" },
-              { title: "Bottom", value: "bottom" },
-            ],
-            layout: "radio",
-          },
-        },
-      ],
-    }),
   ],
 })
-
